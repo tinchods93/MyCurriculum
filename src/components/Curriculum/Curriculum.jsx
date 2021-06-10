d145e6496c0d7f181b153c637cf0d3390bb7b924
tinchods93 <mds.designss@gmail.com>
2021-06-10 00:03:06 -0300

    up

--- src/components/Curriculum/Curriculum.jsx
+++ src/components/Curriculum/Curriculum.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { profile as Profile } from '../../profile.json';
+import { capitalize } from '../../utils/stringUtils';
 import './Curriculum.css';
 
 export default class Curriculum extends Component {
@@ -16,13 +17,10 @@ export default class Curriculum extends Component {
 
   loadProfile = () => {
     this.setState({ profile: Profile });
-    console.log(Profile);
   };
 
   spawnText = (key, title) => {
     const { profile } = this.state;
-    // console.log(profile.about_you);
-    console.log(key);
     const about = {
       title: title,
       body: profile[key].map((item, index) => {
@@ -44,11 +42,11 @@ export default class Curriculum extends Component {
     const keys = Object.keys(profile.social_networks);
     const icons = keys.map((social_network, index) => {
       return social_network === 'linkedin' ? (
-        <a href={profile.social_networks[social_network]}>
+        <a href={profile.social_networks[social_network]} key={index}>
           <i className={`sn-icon fab fa-${social_network}`} />
         </a>
       ) : (
-        <a href={profile.social_networks[social_network]}>
+        <a href={profile.social_networks[social_network]} key={index}>
           <i className={`sn-icon fab fa-${social_network}-square`}></i>
         </a>
       );
@@ -58,9 +56,14 @@ export default class Curriculum extends Component {
 
   render() {
     const { profile } = this.state;
+
+    const personal_data_attributes =
+      profile.personal_data !== undefined
+        ? Object.keys(profile.personal_data)
+        : [];
     return (
       <>
-        {profile.city !== undefined ? (
+        {profile.personal_data !== undefined ? (
           <div className='curriculum-card'>
             <div className='personal-data'>
               <div className='profile-title'>
@@ -68,24 +71,53 @@ export default class Curriculum extends Component {
                 <p>{profile.wanted_job}</p>
               </div>
               <div className='profile-text'>
-                <p>{`${profile.city}, ${profile.country}.`}</p>
+                {personal_data_attributes.map((attr, index) => {
+                  return (
+                    <Fragment key={index}>
+                      <div className='personal-data-container'>
+                        <div className='pdataAttribute'>
+                          <span>{capitalize(attr)}:</span>
+                        </div>
+                        <div className='pdataValue'>
+                          {attr !== 'email' &&
+                          attr !== 'telefono' &&
+                          attr !== 'linkedin' &&
+                          attr !== 'github' &&
+                          attr !== 'hackerRank'
+                            ? capitalize(profile.personal_data[attr])
+                            : profile.personal_data[attr]}
+                        </div>
+                      </div>
+                    </Fragment>
+                  );
+                })}
               </div>
               <div className='profile-text'>{this.spawnSocialNetworks()}</div>
             </div>
             <div className='experience-data'>
-              {this.spawnText('about_you', 'ABOUT ME')}
-              {this.spawnText('experience', 'EXPERIENCE')}
+              {this.spawnText('about_you', 'ACERCA DE MI')}
+              {this.spawnText('experience', 'EXPERIENCIA')}
               {
                 <Item
                   data={profile.education}
-                  title={'EDUCATION'}
+                  title={'EDUCACIÓN'}
                   icon_class={profile.icon_classes.education}
                 />
               }
+              <Skills
+                data={profile.skills}
+                title={'HABILIDADES'}
+                icon_class={profile.icon_classes.skills}
+              />
+              <Skills
+                data={profile.languages}
+                title={'LENGUAJES'}
+                icon_class={profile.icon_classes.languages}
+              />
               {
                 <Item
                   data={profile.certificates}
-                  title={'CERTIFICATES'}
+                  title={'CERTIFICADOS'}
                   icon_class={profile.icon_classes.certificates}
                 />
               }
@@ -118,12 +150,54 @@ const Item = ({ data, title, icon_class }) => {
   return <Section data={_data} icon_class={icon_class} />;
 };
 
+const Skills = ({ data, title, icon_class }) => {
+  const keys = Object.keys(data);
+  const _data = {
+    title: title,
+    body: keys.map((key, index) => {
+      const skillsLevel = [];
+      for (let index = 0; index < 5; index++) {
+        if (index < data[key]) {
+          skillsLevel.push(
+            <i className='fas fa-circle filled' key={index}></i>
+          );
+        } else {
+          skillsLevel.push(<i className='fas fa-circle' key={index}></i>);
+        }
+      }
+      return (
+        <Fragment key={index}>
+          <div className='skillsContainer desc-text'>
+            <div className='skill-title'>
+              <span className='exp-title'>{key}:</span>
+            </div>
+            <div className='skillLevel'>
+              <span>{skillsLevel}</span>
+            </div>
+          </div>
+        </Fragment>
+      );
+    }),
+  };
+  return (
+    <div className='section'>
+      <div className='section-title'>
+        <p>
+          <i className={`fas ${icon_class}`}></i>
+          <span>{_data.title}</span>
+        </p>
+      </div>
+      <div className='section-body skills'>{_data.body}</div>
+    </div>
+  );
+};
+
 const Section = ({ data, icon_class }) => {
   return (
     <div className='section'>
       <div className='section-title'>
         <p>
-          <i class={`fas ${icon_class}`}></i>
+          <i className={`fas ${icon_class}`}></i>
           <span>{data.title}</span>
         </p>
       </div>
